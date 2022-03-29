57dd135720ea684fc9cce93111ed7f778d2f001c
mason <masonwong1999@gmail.com>
2022-03-29 12:58:28 +1100

    added data of weather

--- practice/practice.py
+++ practice/practice.py
@@ -1,21 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-x = np.arange(100, dtype=np.float64) 
+df = pd.read_csv("../data/temp.csv")
+df['date'] = pd.to_datetime(df['date'])
 
-
-fig, ax = plt.subplots()
-ax.plot(x *  np.sin(0.2 * x), x * np.cos(0.2 * x))
-
-ax.set_title('graphs')
-ax.set_xlabel('the x axis')
-ax.set_ylabel('the y axis')
-
-# set limits via tuple
-ax.set_xlim((-10, 10))
-ax.set_ylim((-10, 10))
-
-
-fig.savefig('out.png')
-
-# plt.show()
